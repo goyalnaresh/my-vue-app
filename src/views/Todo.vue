cb2de024e34d6f3f4959d4f1bd641197ed133908
Naresh Goyal <naresh.goyal@nagarro.com>
2018-12-02 18:51:20 +0100

    todolist working
    Signed-off-by: Naresh Goyal <naresh.goyal@nagarro.com>

---
+++ src/views/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <input v-model="newtask">
+    <button @click="addTask">Add</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo">{{todo}}</li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      newtask : '',
+      todos: ["first task", "Second task"]
+    };
+  },
+  methods : {
+      addTask() {
+         this.todos.push(this.newtask),
+         this.newtask = '' 
+      }
+  }
+};
+</script>
